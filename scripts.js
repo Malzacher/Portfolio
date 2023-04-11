6d396c6cc5746fd0aeb70779a61247a7bb4c347b
Nick <s3785693@student.rmit.edu.au>
2023-04-11 15:38:24 +1000

    cools

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 console.log("testing")
 
-$(document).ready(function(){
+$(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $("a").on('click', function (event) {
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
@@ -16,7 +16,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 800, function () {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
@@ -24,3 +24,18 @@ $(document).ready(function(){
     } // End if
   });
 });
+
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "malzacher@gmail.com",
+    Password: "5AE837F92DD55D21B561805A74948DF7C845",
+    To: 'nicholas.malzacher@gmail.com',
+    From: 'nicholas.malzacher@gmail.com',
+    Subject: "You Got an Email from: " + document.getElementById("email").value,
+    Body: "Name: " + document.getElementById("name").value + "<br> Email:" + document.getElementById("email").value + "<br> Subject:" + document.getElementById("subject").value
+  }).then(
+    message => alert(message)
+  );
+}
